323d568b5446c68d2f8f2182233a97e07698163b
Boopington <nicole@bewp.ca>
2018-12-29 21:24:01 -0800

    Clean up comments, add check for admin, change any key pause to enter

--- ramdisk.bat
+++ ramdisk.bat
@@ -1,23 +1,29 @@
 @echo off
-set dir=Paks
-set game="C:\Program Files (x86)\Steam\steamapps\common\PUBG\TslGame\Content"
-set size=7800M
+set dir=EFT
+set game="C:\Battlestate Games"
+set size=13000M
 set ramdisk_drive=X
 
 title Ramdisk for %dir%
+
+net session >nul 2>&1
+if %ERRORLEVEL% equ 0 (
+    echo Hurray, running as admin
+) else (
+    echo Run as admin please
+    pause
+    exit
+)
+
 imdisk -a -o awe -s %size% -m %ramdisk_drive%: -p "/fs:ntfs /q /y"
 cd %game%
 xcopy %dir% %ramdisk_drive%:\%dir% /O /X /E /H /K /I
 ren %dir% %dir%_orig
 mklink /D %dir% %ramdisk_drive%:\%dir%
 cls
-echo Unpause when done playing
-pause
+set /p DUMMY=Hit ENTER when done playing...
 
 rmdir %dir%
 ren %dir%_orig %dir%
 imdisk -D -m %ramdisk_drive%:
 exit
-
-rem xcopy %dir% %ramdisk_drive%:\%dir% /O /X /E /H /K /I
-rem ROBOCOPY %dir% %ramdisk_drive%:\%dir% /E /njh /njs /nc /COPYALL /DCOPY:DAT
